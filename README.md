45d468d41e114f5b8aef183e03a70e185f527e6b
tricia-ferreira <triciaferreirac@gmail.com>
2017-08-19 22:17:21 +0200

    specs and update readme

--- README.md
+++ README.md
@@ -15,10 +15,21 @@ I chose to import through a rake task:
 `rake data:import_loans[path_to_file]`
 
 The reason why I chose this route:
+
 * Scale ability, you can upload 10 or a 1_000 without the server timing out
 * The user does not have to wait for the form to submit
 
+The third party gems I used for testing are:
 
+* `rspec-rails`
+* `shoulda-matchers`
+* `factory_girl_rails`
+* `rails-controller-testing`
 
-* ...
+Third party gem for the app:
+
+* `pry-byebug`, I prefer using pry, I have tried using byebug but it just didn't feel right
+* `jquery-rails`
+* `bootstrap-sass`, I have exclusively worked with bootstrap in the last 4 years
+
 # jumo_loans
--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,9 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
-  it { should respond_to(:msisdn) }
+  it { should respond_to(:contact_number) }
   it { should respond_to(:network) }
   it { should respond_to(:loaned_at) }
   it { should respond_to(:product) }
   it { should respond_to(:amount) }
+
+  context 'scopes' do
+    before :each do
+      create_list(:loan, 2, product: 'Product 1', network: 'Network 2', loaned_at: (Date.today - 2.month))
+      create_list(:loan, 3, product: 'Product 2', network: 'Network 1', loaned_at: (Date.today - 3.month))
+    end
+
+    describe '#aggregate_by_product' do
+      subject { Loan.aggregate_by_product }
+
+      it 'returns records grouped by product' do
+        expect(subject.count).to eq 2
+      end
+
+      it 'returns loans for first product' do
+        @loans = subject.first
+        expect(@loans[1].count).to eq 2
+      end
+    end
+
+    describe '#aggregate_by_network' do
+      subject { Loan.aggregate_by_network }
+
+      it 'returns records grouped by network' do
+        expect(subject.count).to eq 2
+      end
+
+      it 'returns loans for first network' do
+        @loans = subject.first
+        expect(@loans[1].count).to eq 2
+      end
+    end
+
+    describe '#aggregate_by_month' do
+      subject { Loan.aggregate_by_month }
+
+      it 'returns records grouped by month' do
+        expect(subject.count).to eq 2
+      end
+
+      it 'returns loans for first month' do
+        @loans = subject.first
+        expect(@loans[1].count).to eq 2
+      end
+    end
+  end
 end
